f6735f498ebe3550ddde2549b51bebf6cfb01c02
VMwhdwls24 <whdwls24@vaultmicro.com>
2022-07-15 16:06:00 +0900

    7/15 React MovieWebsite feat: 20th Commit 6.2 Deps

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev+1);
-  console.log("I run all time");
-  const YOLO = () => {
-    console.log("I run ONLY once");
-  }
-  useEffect(YOLO, []);
+  const onChange = (e) => setKeyword(e.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
   return (
-    <div className="App">  
+    <div>  
+      <input type="text" 
+        value={keyword}
+        onChange={onChange}
+        placeholder="Search for..."
+      />
       <h1>{counter}</h1> 
       <button onClick={onClick}>Click me</button>
     </div>
