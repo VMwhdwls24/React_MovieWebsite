519cddce7655fca270658d8da636f726f7046e69
VMwhdwls24 <whdwls24@vaultmicro.com>
2022-07-15 16:36:25 +0900

    7/15 React MovieWebsite feat: 21th Commit 6.4 CleanUp

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import Button from "./Button"
 import styles from "./App.module.css"
 import { useState, useEffect } from "react";
+function Hello() {
+  const destroyFn = () => {
+    console.log("Bye");
+  }
+  const effectFn = () => {
+    console.log("Created!");
+    return destroyFn;
+  }
+  useEffect(effectFn, []);
+  return <h1>Hello!</h1>
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev+1);
-  const onChange = (e) => setKeyword(e.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>  
-      <input type="text" 
-        value={keyword}
-        onChange={onChange}
-        placeholder="Search for..."
-      />
-      <h1>{counter}</h1> 
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
