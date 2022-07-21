d9ea7f7676073e1aa8dae7a6501ab7f3736829a6
VMwhdwls24 <whdwls24@vaultmicro.com>
2022-07-21 13:53:19 +0900

    7/21 React MovieWebsite feat: 23th Commit 7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = e => setToDo(e.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [input, setInput] = useState(0);
+  const [dollars, setDollars] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then(json => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const onSelect = e => {
+    setSelectedCoin(e.target.selectedIndex);
+    console.log(coins[selectedCoin].quotes.USD.price);
+  };
+  const onChange = e => {
+    if (e.target.value !== null) setInput(e.target.value);
+  };
   const onSubmit = e => {
     e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos(currentArray => [toDo, ...currentArray]);
+    setDollars(input);
+    console.log(dollars);
   };
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect}>
+          {coins.map(coin => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
+          type="number"
+          step="0.0000000000000001"
+          placeholder="USD to convert..."
         />
-        <button>Add To Do</button>
+        <button>Convert</button>
       </form>
-      <hr />
-
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h3>
+        {loading
+          ? ""
+          : `You can got ${dollars / coins[selectedCoin].quotes.USD.price} 
+        ${coins[selectedCoin].symbol}
+        with your
+        ${dollars}
+        dollars`}
+      </h3>
     </div>
   );
 }
