bdb2f81ec042365054cef9f503a5b361b36cd04d
VMwhdwls24 <whdwls24@vaultmicro.com>
2022-07-21 14:32:31 +0900

    7/21 React MovieWebsite feat: 24th Commit 7.3 Movie App part.1

--- src/App.js
+++ src/App.js
@@ -2,64 +2,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [input, setInput] = useState(0);
-  const [dollars, setDollars] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  const onSelect = e => {
-    setSelectedCoin(e.target.selectedIndex);
-    console.log(coins[selectedCoin].quotes.USD.price);
-  };
-  const onChange = e => {
-    if (e.target.value !== null) setInput(e.target.value);
-  };
-  const onSubmit = e => {
-    e.preventDefault();
-    setDollars(input);
-    console.log(dollars);
-  };
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onSelect}>
-          {coins.map(coin => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          type="number"
-          step="0.0000000000000001"
-          placeholder="USD to convert..."
-        />
-        <button>Convert</button>
-      </form>
-      <h3>
-        {loading
-          ? ""
-          : `You can got ${dollars / coins[selectedCoin].quotes.USD.price} 
-        ${coins[selectedCoin].symbol}
-        with your
-        ${dollars}
-        dollars`}
-      </h3>
     </div>
   );
 }
-
 export default App;
